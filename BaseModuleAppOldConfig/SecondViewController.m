8e96431baae12597fd74afa549db98c0e0dac5c1
zhongxm <samingzhong@foxmail.com>
2022-03-21 17:59:05 +0800

    block与循环引用、NSMutableArray与线程安全。

--- BaseModuleAppOldConfig/SecondViewController.m
+++ BaseModuleAppOldConfig/SecondViewController.m
@@ -7,6 +7,12 @@
 
 #import "SecondViewController.h"
 #import "objc-retainCount/NSObject+GACRetainCount.h"
+#import <ReactiveObjC.h>
+
+
+
+
+
 
 
 void myFunctionWithBlock(dispatch_block_t block) {
@@ -16,8 +22,51 @@ void myFunctionWithBlock(dispatch_block_t block) {
 }
 
 
+@interface MyObject : NSObject
+
+@property (nonatomic, strong) void(^block)(void);
+
+- (void)methodWithTestBlock:(void(^)(void))block;
+
+@end
+
+@implementation MyObject
+
+- (void)methodWithTestBlock:(void(^)(void))block {
+    self.block = block;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if (self.block) {
+            self.block();
+            //self.block = nil;// 打破循环引用
+        }
+    });
+}
+
+- (void)methodWithTestBlock2:(void(^)(void))block {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if (block) {
+            block();
+        }
+    });
+}
+
+- (void)methodWithTestBlock3:(void(^)(void))block {
+    self.block = block;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if (self.block) {
+            self.block();
+        }
+    });
+}
+
+@end
+
+
+
 
 @interface SecondViewController ()
+
+@property (nonatomic, strong) MyObject *myObj;
 
 @end
 
@@ -33,33 +82,77 @@ void myFunctionWithBlock(dispatch_block_t block) {
     // str是一个autorelease对象，设置一个weak的引用来观察它
     reference = str;
     
-//    __weak __typeof(self)weakSelf = self;
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        __strong __typeof(weakSelf) strongSelf = weakSelf;
-//        NSLog(@"retainCount:%ld", [strongSelf rq_retainCount]);
-//        NSLog(@"self:%@", strongSelf);
-//    });
-//
-//
-//
-//
-//
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        __strong __typeof(weakSelf) strongSelf = weakSelf;
-//
-//        NSLog(@"self:%@", strongSelf);
-//    });
-    
     myFunctionWithBlock(^{
         NSLog(@"self :%@", self);
     });
+
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        NSLog(@"retainCount:%ld", [self rq_retainCount]);
-        [self.navigationController popViewControllerAnimated:YES];
-    });
+//    [self blockTest];
+    [self threadSafeCase];
     
 }
+
+
+- (void)threadSafeCase {
+//    NSMutableArray *arr = [NSMutableArray arrayWithCapacity:100];
+    NSMutableArray *arr = @[].mutableCopy;
+    for (int i=0; i<100; i++) {
+        dispatch_async(dispatch_queue_create("123", DISPATCH_QUEUE_CONCURRENT), ^{
+            [arr addObject:[NSObject new]];
+        });
+    }
+}
+
+
+- (void)blockTest {
+    
+    self.myObj = MyObject.new;
+    [self.myObj methodWithTestBlock:^{
+        NSLog(@"self:%@", self);
+    }];
+    
+//    [self.myObj methodWithTestBlock2:^{ //
+//        NSLog(@"self:%@", self);
+//    }];
+    
+    
+//    [self weakStrongDanceCase];
+    
+//    [self RACWeakifyStrongifyCase];
+    
+//    [self RACObserveCase];
+}
+
+- (void)RACObserveCase {
+    @weakify(self);
+    [RACObserve(self, myObj) subscribeNext:^(id  _Nullable x) {
+        @strongify(self);
+        NSLog(@"hello:%@", self.myObj);
+    }];
+}
+
+-(void)weakStrongDanceCase {
+    self.myObj = [MyObject new];
+    __weak __typeof(self)weakSelf = self;
+    [self.myObj methodWithTestBlock:^{
+        __strong __typeof(weakSelf) strongSelf = weakSelf; // 此处因为只是引用了weakSelf 所以不会增加self的引用计数。
+        NSLog(@"hello:%@", strongSelf);
+    }];
+}
+
+- (void)RACWeakifyStrongifyCase {
+    self.myObj = [MyObject new];
+    __weak __typeof(self)weakSelf = self;
+    [self.myObj methodWithTestBlock:^{
+        __strong __typeof(weakSelf) self = weakSelf; // block中允许我们重定义self
+        NSLog(@"hello:%@", self);
+    }];
+    
+    //__strong __typeof(weakSelf) self = weakSelf;//Redefinition of 'self'
+}
+
+
+
 
 - (void)dealloc {
     NSLog(@"dealloc ");
